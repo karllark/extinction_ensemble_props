0a7db8fc6c5200006dfc6695abadb0dafaf388ac
Karl Gordon <kgordon@stsci.edu>
2026-06-26 13:40:15 -0400

    adding in option for FM90 parameters / AV instead of / EBV

--- extinction_ensemble_props/helpers.py
+++ extinction_ensemble_props/helpers.py
@@ -7,7 +7,9 @@ __all__ = ["poss_params", "param_labels", "ptypes", "get_dataset"]
 # possible parameters
 # fmt: off
 poss_params = ["AV", "EBV", "RV", "IRV", "NHI", "NHI_EBV", "NHI_AV",
-               "C1", "C2", "C3", "B3", "C4", "x0", "gamma", "metallicity"]
+               "C1", "C2", "C3", "B3", "C4", "x0",
+               "C1_AV", "C2_AV", "C3_AV", "B3_AV", "C4_AV",
+               "gamma", "metallicity"]
 # fmt: on
 
 # parameter labels
@@ -19,10 +21,14 @@ param_labels = {
     "NHI": r"$N(HI)$ [$10^{21}~H~cm^{-2}$]",
     "NHI_EBV": r"$N(HI)/E(B-V)$ [$10^{21}~H~cm^{-2}~mag^{-1}$]",
     "NHI_AV": r"$N(HI)/A(V)$ [$10^{21}~H~cm^{-2}~mag^{-1}$]",
-    "C1": "$C_1$ = UV intercept",
-    "C2": "$C_2$ = UV slope",
-    "B3": "$B_3$ = bump amplitude",
-    "C4": "$C_4$ = FUV rise amplitude",
+    "C1": "$C_1$ = UV intercept / $E(B-V)$",
+    "C1_AV": "$C_1$ = UV intercept / $A(V)$",
+    "C2": "$C_2$ = UV slope / $E(B-V)$",
+    "C2_AV": "$C_2$ = UV slope / $A(V)$",
+    "B3": "$B_3$ = bump amplitude / $E(B-V)$",
+    "B3_AV": r"$B_3^{A(V)}$ = bump amplitude / $A(V)$",
+    "C4": "$C_4$ = FUV rise amplitude / $E(B-V)$",
+    "C4_AV": "$C_4$ = FUV rise amplitude / $A(V)$",
     "x0": r"$x_o$ = bump center [$\mu \mathrm{m}^{-1}$]",
     "gamma": r"$\gamma$ = bump width [$\mu \mathrm{m}^{-1}$]",
     "metallicity": r"Z/Z$_\odot$",
@@ -48,16 +54,17 @@ ptypes = {
     "G25_wisci": ("bo", basealpha, "MW: G25 WISCI"),
 }
 
-# ptypes = {
-#     "G03_lmc": (("tab:orange", ">"), 0.1, "LMC: G03"),
-#     "GCC09": ("kD", 0.1, "MW: GCC09"),
-#     "G24_smc": ("b<", 0.1, "SMC: G24"),
-#     "C25_m31": ("rs", 0.75, "M31"),
-#     "C25_m31_blue": ("bs", 0.75, "M31: Blue"),
-#     "C25_m31_green": ("go", 0.75, "M31: Green"),
-#     "C25_m31_red": ("rD", 0.75, "M31: Red"),
-#     "C25_m31_black": ("kv", 0.75, "M31: Black"),
-# }
+
+def _div_frac_unc(tdata, x, y):
+    """
+    Fractional uncertainty of x/y
+    """
+    frac_unc = np.zeros(len(tdata))
+    gvals = (tdata[x] != 0.0) & (tdata[y] != 0.0)
+    frac_unc[gvals] = (tdata[f"{x}_unc"][gvals] / tdata[x][gvals]) ** 2 + (
+        tdata[f"{y}_unc"][gvals] / tdata[y][gvals]
+    ) ** 2
+    return np.sqrt(frac_unc)
 
 
 def get_dataset(cset):
@@ -82,6 +89,19 @@ def get_dataset(cset):
             f"{data_path}/{cset}_ensemble_params.dat", format="ascii.ipac"
         )
 
+    # add EBV is not present and AV, RV are present
+    if (
+        ("AV" in tdata.colnames)
+        & ("RV" in tdata.colnames)
+        & ("EBV" not in tdata.colnames)
+    ):
+        tdata["EBV"] = tdata["AV"] / tdata["RV"]
+        tdata["EBV_unc"] = tdata["EBV"] * _div_frac_unc(tdata, "AV", "RV")
+
+    if "IRV" not in tdata.colnames:
+        tdata["IRV"] = 1.0 / tdata["RV"]
+        tdata["IRV_unc"] = tdata["IRV"] * tdata["RV_unc"] / tdata["RV"]
+
     # now add data if missing and derivable from expected columns
     if "B3" not in tdata.colnames:
         tdata["B3"] = tdata["C3"] / (tdata["gamma"] ** 2)
@@ -93,37 +113,59 @@ def get_dataset(cset):
             # C3 and gamma strongly correlated
             tdata["B3_unc"] *= 0.2
 
-    if "IRV" not in tdata.colnames:
-        tdata["IRV"] = 1.0 / tdata["RV"]
-        tdata["IRV_unc"] = tdata["IRV"] * tdata["RV_unc"] / tdata["RV"]
+    # add in FM90 parameters normalized by AV instead of EBV if not present
+    # check only for B3 and assume the rest will be there
+    # see Gordon et al. 2009 for the equations converting standard FM90
+    #    parameters to normalized by A(V) versions
+    if (
+        ("B3" in tdata.colnames)
+        & ("RV" in tdata.colnames)
+        & ("B3_AV" not in tdata.colnames)
+    ):
+        if "C1" in tdata.colnames:
+            tdata["C1_AV"] = tdata["C1"] / tdata["RV"] + 1.0
+            tdata["C1_AV_unc"] = np.absolute(
+                tdata["C1_AV"] * _div_frac_unc(tdata, "C1", "RV")
+            )
+        tdata["C2_AV"] = tdata["C2"] / tdata["RV"]
+        tdata["C2_AV_unc"] = np.absolute(
+            tdata["C2_AV"] * _div_frac_unc(tdata, "C2", "RV")
+        )
+        if "C3" in tdata.colnames:
+            tdata["C3_AV"] = tdata["C3"] / tdata["RV"]
+            tdata["C3_AV_unc"] = np.absolute(
+                tdata["C3_AV"] * _div_frac_unc(tdata, "C3", "RV")
+            )
+        tdata["B3_AV"] = tdata["B3"] / tdata["RV"]
+        tdata["B3_AV_unc"] = np.absolute(
+            tdata["B3_AV"] * _div_frac_unc(tdata, "B3", "RV")
+        )
+        tdata["C4_AV"] = tdata["C4"] / tdata["RV"]
+        tdata["C4_AV_unc"] = np.absolute(
+            tdata["C4_AV"] * _div_frac_unc(tdata, "C4", "RV")
+        )
 
-    # divide by 10^21 to make easier to undertand and fit numbers
+    # divide by 10^21 to make easier to understand and fit numbers
     if "NHI" in tdata.colnames:
         tdata["NHI"] /= 1e21
         tdata["NHI_unc"] /= 1e21
 
+    # add NHI_EBV
     if (
         ("NHI" in tdata.colnames)
         & ("EBV" in tdata.colnames)
         & ("NHI_EBV" not in tdata.colnames)
     ):
         tdata["NHI_EBV"] = tdata["NHI"] / tdata["EBV"]
-        tdata["NHI_EBV_unc"] = (tdata["NHI_unc"] / tdata["NHI"]) ** 2 + (
-            tdata["EBV_unc"] / tdata["EBV"]
-        ) ** 2
-        tdata["NHI_EBV_unc"] = tdata["NHI_EBV"] * np.sqrt(tdata["NHI_EBV_unc"])
+        tdata["NHI_EBV_unc"] = tdata["NHI_EBV"] * _div_frac_unc(tdata, "NHI", "EBV")
 
+    # add NHI_AV
     if (
         ("NHI" in tdata.colnames)
         & ("AV" in tdata.colnames)
         & ("NHI_AV" not in tdata.colnames)
     ):
         tdata["NHI_AV"] = tdata["NHI"] / tdata["AV"].value
-        tdata["NHI_AV_unc"] = 0.0 * tdata["NHI_AV"]
-        gvals = tdata["NHI"] > 0.0
-        tdata["NHI_AV_unc"][gvals] = (
-            tdata["NHI_unc"][gvals] / tdata["NHI"][gvals]
-        ) ** 2 + (tdata["AV_unc"][gvals] / tdata["AV"][gvals]) ** 2
-        tdata["NHI_AV_unc"] = tdata["NHI_AV"] * np.sqrt(tdata["NHI_AV_unc"])
+        tdata["NHI_AV_unc"] = tdata["NHI_AV"] * _div_frac_unc(tdata, "NHI", "AV")
 
     return tdata
